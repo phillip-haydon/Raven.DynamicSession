97bce1a535abb24f64fb86ed322007c158bfdd30
Phillip Haydon <phillip.haydon@gmail.com>
2012-09-29 01:44:17 +0800

    Updated naming and wrote the README

--- src/Raven.DynamicSession.TestConsole/Program.cs
+++ src/Raven.DynamicSession.TestConsole/Program.cs
@@ -7,47 +7,47 @@ namespace Raven.DynamicSession.TestConsole
     {
         static void Main(string[] args)
         {
-            var documentStore = (new DocumentStore()
+            var documentStore = (new DocumentStore
             {
                 DefaultDatabase = "Testing",
                 Url = "http://localhost:8080"
             }).Initialize();
 
-            using (dynamic chainer = documentStore.OpenDynamicSession())
+            using (dynamic session = documentStore.OpenDynamicSession())
             {
-                chainer.Posts.insert(new
+                session.Posts.insert(new
                 {
                     Name = "Rabbit"
                 }, "909");
-                chainer.Posts.Insert(new
+                session.Posts.Insert(new
                 {
                     Name = "Banana"
                 }, "123");
-                chainer.People.insert(new
+                session.People.insert(new
                 {
                     FirstName = "Phillip"
                 }, "1");
-                chainer.People.Insert(new
+                session.People.Insert(new
                 {
                     FirstName = "Prabir"
                 }, "2");
 
-                chainer.SaveChanges();
+                session.SaveChanges();
             }
 
-            using (dynamic chainer = documentStore.OpenDynamicSession())
+            using (dynamic session = documentStore.OpenDynamicSession())
             {
-                dynamic result = chainer.Posts.load(123);
-                Console.WriteLine(result.Name);
+                dynamic post1 = session.Posts.load(123);
+                Console.WriteLine(post1.Name);
 
-                dynamic result2 = chainer.Posts.Load(909);
-                Console.WriteLine(result2.Name);
+                dynamic post2 = session.Posts.Load(909);
+                Console.WriteLine(post2.Name);
 
-                dynamic result3 = chainer.People.load(1);
-                Console.WriteLine(result3.FirstName);
+                dynamic person1 = session.People.load(1);
+                Console.WriteLine(person1.FirstName);
 
-                dynamic result4 = chainer.People.Load(2);
-                Console.WriteLine(result4.FirstName);
+                dynamic person2 = session.People.Load(2);
+                Console.WriteLine(person2.FirstName);
             }
 
             Console.ReadKey();
