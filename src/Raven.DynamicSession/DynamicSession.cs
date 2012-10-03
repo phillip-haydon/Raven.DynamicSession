b648c382e78f51d011b524f11efa60744f008f55
Phillip Haydon <phillip.haydon@gmail.com>
2012-10-04 00:52:56 +0800

    removed dynamic, reworked ifs, implemented 'all'

--- src/Raven.DynamicSession/DynamicSession.cs
+++ src/Raven.DynamicSession/DynamicSession.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Dynamic;
 using System.Linq;
-using System.Reflection;
 using Raven.Client;
 
 namespace Raven.DynamicSession
@@ -32,8 +31,6 @@ namespace Raven.DynamicSession
             private IDocumentSession Session { get; set; }
             private string CollectionName { get; set; }
 
-            private static readonly Type documentSessionType = typeof(IDocumentSession);
-
             public ChainBuilder(DynamicObject originalObject, IDocumentSession session, string collection)
             {
                 OriginalObject = originalObject;
@@ -43,63 +40,49 @@ namespace Raven.DynamicSession
 
             public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
             {
-                if (binder.Name.Equals("load", StringComparison.OrdinalIgnoreCase))
+                switch (binder.Name.ToLower())
                 {
-                    var generic = GetLoadMethod();
+                    case "load":
+                    case "get":
+                    {
+                        Guid guidId;
+                        if (Guid.TryParse(args[0].ToString(), out guidId))
+                        {
+                            result = Session.Load<dynamic>(guidId);
+                        }
+                        else
+                        {
+                            var id = CollectionName.ToLower() + "/" + args[0];
+                            result = Session.Load<dynamic>(id);
+                        }
 
-                    var id = CollectionName.ToLower() + "/" + args[0];
+                        return true;
+                    }
+                    case "insert":
+                    {
+                        var objectToStore = args[0];
+                        var id = CollectionName.ToLower() + "/" + args[1];
 
-                    result = generic.Invoke(Session, new object[] { id });
-                    return true;
-                }
+                        Session.Store(objectToStore, id);
 
-                if (binder.Name.Equals("insert", StringComparison.OrdinalIgnoreCase))
-                {
-                    MethodInfo method = GetStoreMethod();
+                        var metadata = Session.Advanced.GetMetadataFor(objectToStore);
+                        metadata["Raven-Entity-Name"] = CollectionName;
+                        metadata[DynamicClrTypePlaceHolder] = CollectionName;
 
-                    var id = CollectionName.ToLower() + "/" + args[1];
-                    var objectToStore = args[0];
+                        result = objectToStore;
+                        return true;
+                    }
+                    case "all":
+                    {
+                        result = Session.Advanced.LuceneQuery<dynamic>()
+                                        .WhereEquals("@metadata.Raven-Entity-Name", CollectionName)
+                                        .ToList();
 
-                    method.Invoke(Session, new[] { objectToStore, id });
-
-                    var metadata = Session.Advanced.GetMetadataFor(objectToStore);
-                    metadata["Raven-Entity-Name"] = CollectionName;
-                    metadata[DynamicClrTypePlaceHolder] = CollectionName;
-
-                    result = null;
-                    return true;
+                        return true;
+                    }
                 }
 
                 return base.TryInvokeMember(binder, args, out result);
-            }
-
-            private static MethodInfo GetLoadMethod()
-            {
-                var method = documentSessionType.GetMethods()
-                                                .FirstOrDefault(x => x.Name == "Load")
-                                                .MakeGenericMethod(typeof(object));
-
-                return method;
-            }
-
-            private static MethodInfo GetStoreMethod()
-            {
-                return documentSessionType.GetMethods()
-                    .Where(x =>
-                    {
-                        if (x.Name != "Store")
-                            return false;
-
-                        var methodParams = x.GetParameters();
-                        if (methodParams.Length != 2)
-                            return false;
-
-                        if (methodParams[1].ParameterType == typeof(string))
-                            return true;
-
-                        return false;
-                    })
-                    .FirstOrDefault();
             }
         }
 
